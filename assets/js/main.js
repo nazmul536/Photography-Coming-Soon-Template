d50dddaf8df63a9186981d7a4283bed31274866c
nazmul128 <shafayat536@gmail.com>
2022-05-08 16:00:31 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.dicover-carousel-area').owlCarousel({ 
+        loop:true,
+        margin:10,
+        nav:false,
+        items: 1,
+        autoplay: true
+    })
+})
